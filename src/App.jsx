77cfe661f1ca5ea295ed75cf46d86ef9bd7a3cfc
Thun-2004 <66011276@kmitl.ac.th>
2024-10-06 22:29:37 +0700

    done structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import DashBoard from './pages/Main/Dashboard'
 import RateAndReview from './pages/Others/RateAndReview'
 import Menu from './pages/Restaurant/Restaurant'
+import Authentication from './pages/Others/Authentication'
 
 const App = () => {
   return (
     <>
-      <DashBoard></DashBoard>
+      {/* <DashBoard></DashBoard> */}
       {/* <RateAndReview></RateAndReview> */}
       {/* <Menu restaurantID={8}></Menu> */}
+      <Authentication/>
     </>
   )
 }
---
+++ src/pages/Others/Authentication.jsx
@@ -0,0 +1,26 @@
+import Signup from '../../components/Authentication/Signup';
+import Login from '../../components/Authentication/Login';
+
+const Authentication = () => {
+    return (
+        <div className='flex justify-center object-cover'>
+            <img className="w-screen fixed top-0 blur-sm z-0" src="./loginBg.jpg"></img>
+            {/* <div className="w-2/4 h-4/6 flex absolute top-32 rounded-2xl  z-10"> */}
+            <div className="w-[900px] h-[600px] flex absolute top-36 rounded-2xl  z-10">
+                <div className="w-full h-full shadow-lg">
+                    <div className="absolute w-full h-full opacity-80 bg-white rounded-md z-20">
+                        
+                    </div>
+                    {/* <div className="absolute w-1/2 h-full left-1/2">
+                    </div> */}
+                    {/* <Signup /> */}
+
+                    <Login/>
+                    <img className="absolute w-1/2 h-full p-3 z-30" src="./Authen.png" alt="" />
+                </div>  
+            </div>
+        </div>
+    )   
+}
+
+export default Authentication; 
