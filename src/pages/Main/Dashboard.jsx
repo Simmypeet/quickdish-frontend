cd675db4193d1af26f834f27c9131919f18d6e12
Simmypeet <simmypeet@outlook.com>
2024-11-07 21:41:52 +0700

    make dashboard body take full height

--- src/pages/Main/Dashboard.jsx
+++ src/pages/Main/Dashboard.jsx
@@ -6,10 +6,9 @@
 // import MyProfile from "./MyProfile";
 // import Favourite from "./Favourite";
 // import Notification from "./Notification";
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import BottomBar from "../../components/BottomBar";
 // import RequireAuth from "../../components/RequireAuth";
-
 
 // //sidebar, header, link to other pages
 // /**
@@ -26,13 +25,13 @@
 //         <>
 //             <div className="flex w-full h-svh overflow-y-auto ">
 //                 <div className="
-//                     sticky top-0 left-0 h-svh max-h-svh p-4 hidden md:block 
+//                     sticky top-0 left-0 h-svh max-h-svh p-4 hidden md:block
 //                     z-10
 //                 ">
 //                     <SideBar />
 //                 </div>
 
-//                 <div 
+//                 <div
 //                     className="flex flex-col w-0 grow"
 //                 >
 //                     <div className="grow">
@@ -49,7 +48,7 @@
 //                                 <Route path="/" element={<Home/>}/>
 
 //                                 {/* for several roles = ['user', 'admin', 'staff'] */}
-//                                 <Route element={<RequireAuth allowedRoles={'user'}/>}> 
+//                                 <Route element={<RequireAuth allowedRoles={'user'}/>}>
 //                                     <Route path="purchase_history" element={<PurchaseHistory/>}/>
 //                                     <Route path="myprofile" element={<MyProfile/>}/>
 //                                     <Route path="favourites" element={<Favourite/>}/>
@@ -67,71 +66,83 @@
 //                     <BottomBar />
 //                 </div>
 //             </div>
-//         </> 
+//         </>
 //     )
-// }; 
-
+// };
 
 // export default Dashboard;
 
-import React from "react";
-import SideBar from "../../components/SideBar";
-import Header from "../../components/Header";
-import Home from "./Home";
-import PurchaseHistory from "./PurchaseHistory";
-import MyProfile from "./MyProfile";
-import Favourite from "./Favourite";
-import Notification from "./Notification";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import BottomBar from "../../components/BottomBar";
-import RequireAuth from "../../components/RequireAuth";
+import React from 'react';
+import SideBar from '../../components/SideBar';
+import Header from '../../components/Header';
+import Home from './Home';
+import PurchaseHistory from './PurchaseHistory';
+import MyProfile from './MyProfile';
+import Favourite from './Favourite';
+import Notification from './Notification';
+import { Route, Routes } from 'react-router-dom';
+import BottomBar from '../../components/BottomBar';
+import RequireAuth from '../../components/RequireAuth';
 
 //sidebar, header, link to other pages
 const Dashboard = () => {
     return (
-        <>
-            <div className="flex w-full h-svh overflow-y-auto ">
-                <div className="
-                    sticky top-0 left-0 h-svh max-h-svh p-4 hidden md:block 
-                    z-10
-                ">
-                    <SideBar />
-                </div>
-
-                <div 
-                    className="flex flex-col w-0 grow"
-                >
-                    <div className="grow">
-                        <Routes>
-                            <Route path="/" element={
-                                <div className='md:-ml-4 sticky top-0 z-10'>
-                                    <Header/>
+        <div className="flex h-svh w-full overflow-y-auto">
+            <div
+                className="
+                    sticky left-0 top-0 z-10 hidden h-svh max-h-svh p-4 
+                    md:block
+                "
+            >
+                <SideBar />
+            </div>
+            <div className="flex h-full w-0 grow flex-col">
+                <div className="flex grow flex-col">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <div className="sticky top-0 z-10 md:-ml-4">
+                                    <Header />
                                 </div>
-                            }/>
+                            }
+                        />
+                    </Routes>
+
+                    <div className="h-full grow p-2 md:pl-0 md:pr-4">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+
+                            {/* for several roles = ['user', 'admin', 'staff'] */}
+                            <Route
+                                element={<RequireAuth allowedRoles={'user'} />}
+                            >
+                                <Route
+                                    path="purchase_history"
+                                    element={<PurchaseHistory />}
+                                />
+                                <Route
+                                    path="myprofile"
+                                    element={<MyProfile />}
+                                />
+                                <Route
+                                    path="favourites"
+                                    element={<Favourite />}
+                                />
+                                <Route
+                                    path="notification"
+                                    element={<Notification />}
+                                ></Route>
+                            </Route>
                         </Routes>
-
-                        <div className="p-2 md:pr-4 md:pl-0">
-                            <Routes>
-                                <Route path="/" element={<Home/>}/>
-
-                                {/* for several roles = ['user', 'admin', 'staff'] */}
-                                <Route element={<RequireAuth allowedRoles={'user'}/>}> 
-                                    <Route path="purchase_history" element={<PurchaseHistory/>}/>
-                                    <Route path="myprofile" element={<MyProfile/>}/>
-                                    <Route path="favourites" element={<Favourite/>}/>
-                                    <Route path="notification" element={<Notification/>}></Route>
-                                </Route>
-
-                            </Routes>
-                        </div>
                     </div>
-                    <div className="sticky bottom-0 md:hidden">
-                        <BottomBar />
-                    </div>
+                </div>
+                <div className="sticky bottom-0 md:hidden">
+                    <BottomBar />
                 </div>
             </div>
-        </> 
-    )
-}; 
+        </div>
+    );
+};
 
 export default Dashboard;
