c35899f10774847ff3bfdd7da2a40187f29efc01
Thun-2004 <66011276@kmitl.ac.th>
2024-11-14 17:12:25 +0700

    add loading effect

--- src/api/canteenApi.js
+++ src/api/canteenApi.js
@@ -16,8 +16,20 @@ export const getNearestCanteen = async (userLocation) => { //latitute and longit
 
 }
 
-export const getRestaurantsFromCanteenId = async (canteenId) => {
-
+export const getCanteenFromRestaurantId = async (resId) => {
+    // const response = await axios.get(
+    //     process.env.QUICKDISH_BACKEND_URL + `/canteens/restaurants/${resId}`
+    // ); 
+    const response = await axios.get(`http://127.0.0.1:8000/canteens/restaurants/${resId}`
+    ); 
+    if (response.status !== 200){
+        throw new Error(
+            `Error fetching restaurant data status: ${response.status};
+            body: ${response.data}`
+        ); 
+    };
+    console.log("canteen response: ", response.data); 
+    return response.data; 
 }
 
 //not done
--- src/pages/Main/PurchaseHistory.jsx
+++ src/pages/Main/PurchaseHistory.jsx
@@ -1,46 +1,56 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PurchaseCard from '../../components/PurchaseCard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClockRotateLeft } from '@fortawesome/free-solid-svg-icons';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import { dateFormatted,timeFormatted } from '../../utils/datetimeFormatter';
+import { getCanteenFromRestaurantId } from '../../api/canteenApi';
+import { getRestaurant } from '../../api/restaurantApi';
 
 const PurchaseHistory = () => {
     //try call api and manage 
-    const [color, setColor] = React.useState("green");
+    const [state, setState] = React.useState("ORDERED");
     const [orders, setOrders] = React.useState([]);
     const axiosPrivate = useAxiosPrivate();
-    const handleState = (color) => {
-        setColor(color);
+    const [loading, setLoading] = React.useState(true);
+    const handleState = (status) => {
+        setState(status);
     }
 
-    
-
     const fetchAllOrder = async (status) => {
+        handleState(status);
         try{
             const orders_raw = []; 
             const response = await axiosPrivate.get(`http://127.0.0.1:8000/orders/?status=${status}`); 
             for(let order of response.data){
                 let temp = {}; 
                 const datetime = new Date(order.ordered_at * 1000);
+                const restaurant_response = await getRestaurant(order.restaurant_id); 
+                const canteen_response = await getCanteenFromRestaurantId(order.restaurant_id); //call cabteen details from restaurant_id
                 temp["date"] = dateFormatted(datetime); 
                 temp["time"] = timeFormatted(datetime);
-                temp["restaurant_id"] = order.restaurant_id;  //call restaurant details from restaurant_id
-                temp["menu_id"] = order.items[0]["menu_id"]; //call menu details from menu_id
+                temp["restaurant_id"] = order.restaurant_id; 
+                temp["restaurant_name"] = restaurant_response.name;
+                temp["canteen_name"] = canteen_response.name; 
+                temp["menu_id"] = order.items[0]["menu_id"]; 
                 temp["price_paid"] = order.price_paid; 
                 temp["order_status"] = status; 
                 orders_raw.push(temp);
-
-                for(let order of orders_raw){
-                    console.log(order);
-                }
             }
+            
             setOrders(orders_raw);
         }catch(error){
             console.error("Error fetching orders:", error);
+        }finally{
+            setLoading(false);
         }
-        
     }
+
+    useEffect(() => {
+        // Run fetchAllOrder("ORDERED") when component mounts
+        fetchAllOrder("ORDERED");
+    }, []);
+
     
     return (
         <div className="p-7">
@@ -53,39 +63,44 @@ const PurchaseHistory = () => {
                 <div className="flex justify-between space-x-10 mb-3">
                     <button type="button" onClick={() => fetchAllOrder("ORDERED")}>
                         {/* CANCELLED, SETTLED, PREPARING */}
-                        <h2 className='sub-heading-font active:text-blue-600'>Ongoing</h2>
+                        <h2 className={`sub-heading-font ${state === "ORDERED" ? "text-blue-600" : "text-black"}`}>Ongoing</h2>
                     </button>
-                    <button type="button">
-                        <h2 className='sub-heading-font active:text-green-600'>Completed</h2>
+                    <button type="button" onClick={() => fetchAllOrder("SETTLED")}> {/*edit  */}
+                        <h2 className={`sub-heading-font ${state === "SETTLED" ? "text-green-600" : "text-black"}`}>Completed</h2>
+
                     </button>
-                    <button type="button">
-                        <h2 className='sub-heading-font active:text-red-600'>Canceled/Failed</h2>
+                    <button type="button" onClick={() => fetchAllOrder("CANCELLED")}> {/*edit  */}
+                        <h2 className={`sub-heading-font ${state === "CANCELLED" ? "text-red-600" : "text-black"}`}>Canceled/Failed</h2>
+
                     </button>
                 </div>
                 <hr className="border-t-2 border-gray-300 mb-3"/>
-              {
-                orders.length > 0 ?
-                    orders.map((order, index) => ( 
-                        <PurchaseCard
-                        key={index}
-                        date={order.date}
-                        time={order.time}
-                        name="Mc donalds"
-                        price={order.price_paid}
-                        canteen="Prathep canteen"
-                        userLocation="Building A"
-                        orderStatus={order.order_status}/>
-                        
-                    )) : <h1>No Order history found</h1>
-            }
-                {/* {/* // <PurchaseCard date="1 Sep 24" time="11:09" name="Mc donalds" price="93" canteen="Prathep canteen" userLocation="Building A" orderStatus="Order completed"/> */}
-
+                { loading ? (
+                    <div className="flex justify-center items-center h-48">
+                        <img src="./loading_main.svg" className='w-28'></img>
+                    </div>
+                ) : (
+                    orders.length > 0 ? (
+                        orders.map((order, index) => ( 
+                            <PurchaseCard
+                            key={index}
+                            date={order.date}
+                            time={order.time}
+                            name={order.restaurant_name}
+                            price={order.price_paid}
+                            canteen={order.canteen_name}
+                            userLocation={order.canteen_name}
+                            orderStatus={order.order_status}/>
+                            
+                        ))
+                    ) : (<img src="./no-history.png" className="w-60 h-72" alt="" />)
+                )}
             </div>
-
-            {/* date, time, restaurant name, price, canteen, order status
-            ordered_at,    Restaurant_id, menu_id, price_paid, order_status */}
         </div>
     )
 }
 
+//add restaurant img
+//no history image if no history
+//initially load the ongoing order 
 export default PurchaseHistory;
